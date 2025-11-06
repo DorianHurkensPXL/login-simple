8366190748734b83d42b947005f8a9c7ca7c9d60
DorianHurkensPXL <dorian.hurkens@student.pxl.be>
2025-11-06 13:06:34 +0100

    DEMO-01

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string password;
+Console.WriteLine("Geef je gebruikersnaam in: ");
+string username = Console.ReadLine();
+Console.WriteLine(username);
+Console.ReadKey();
