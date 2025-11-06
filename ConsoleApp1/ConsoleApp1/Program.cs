d05f5af906be28108631b49e721e06afda522804
DorianHurkensPXL <dorian.hurkens@student.pxl.be>
2025-11-06 13:29:55 +0100

    DEMO-05

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,15 +1,31 @@
-﻿int counter = 0;
+﻿int counter = 1;
+bool login = false;
+string adminUser = "jef";
+string adminPassword = "wachtwoord";
 Console.Write("Geef je gebruikersnaam in: ");
 string username = Console.ReadLine();
 Console.Write("Geef je wachtwoord in: ");
 string password = Console.ReadLine();
-while(counter < 3 || (username != "jef" && password != "wachtwoord"))
+if (username == "jef" && password == "wachtwoord")
 {
-    Console.WriteLine("Foutieve login. Probeer opnieuw.");
+    login = true;
+}
+while (counter < 3 && login == false)
+{
+    Console.WriteLine($"Foutieve login. Probeer opnieuw. Nog {3 - counter} pogingen over.");
     Console.Write("Geef je gebruikersnaam in: ");
     username = Console.ReadLine();
     Console.Write("Geef je wachtwoord in: ");
     password = Console.ReadLine();
+    if (username == "jef" && password == "wachtwoord")
+    {
+        login = true;
+    }
     counter++;
 }
+if (login == false)
+{
+    Console.WriteLine("3 foute pogingen, sluit de app af.");
+}
+else { Console.WriteLine("Welkom Jef"); }
 Console.ReadKey();
--- ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cf692cbba438659288ac4f79a620950f275cbb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28ce95d40eb828811e87b80b69abd8301e432fb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
